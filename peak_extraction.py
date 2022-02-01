c70bb7a8f6e7fc8db96bdc1a23bc410580967eef
mo11 <mo11@sanger.ac.uk>
2022-02-01 17:34:11 +0000

    Peak_Extraction_altered to handle multi frequencies.

--- peak_extraction.py
+++ peak_extraction.py
@@ -24,20 +24,22 @@ from sklearn.metrics import r2_score
 
 
 def get_injury_time(d,inp):
-
-    yhat = pd.Series(savgol_filter(d, 11, 4))
+    filter_width=4
+    yhat = pd.Series(savgol_filter(d, 11, filter_width))
     f = pd.Series(yhat)
     f.index = d.index
+    # f.plot()
     d=f
+    injury_time_start=0
     # Start at the min point and step backwards untill the 
     d_min = d[d == d.min()].index[0]
     if inp=='min':
-        for i in range(d_min, 0, -1):
+        for i in range(d_min-filter_width, 0, -1):
             injury_time_start = i
             if (all(list(d[i]>d.loc[i-5:i-1]))): 
                 break
     else:
-        for i in range(d_min, 0, -1):
+        for i in range(d_min-filter_width, 0, -1):
             injury_time_start = i
             if (all(list(d[i]>d.loc[i-20:i-1]))): 
                 break    
@@ -156,13 +158,12 @@ def invert(d1):
         d1[i]=val1
     return d1
     
-
 def peak_detection(d1):
     min_value = d1[d1==d1.min()].index[0]
     min1 = min_value-200
     if(min1<10):
         min1=10
-    d=d1.iloc[min1:min_value+200]
+    d=d1.iloc[min1:min_value+400]
     # d.plot()
     injury_time_start = get_injury_time(d,'min')-5
     d_reverse = d.iloc[::-1]
@@ -176,141 +177,144 @@ def main():
     print('ļets do some PCA')
     df = pd.DataFrame()
     type = 'peak' #or slope
-    treatment='Thrombin' #Thrombin|Wounding|Prolif
+    treatment='Wounding' #Thrombin|Wounding|Prolif
     measurement = 'Resistance' # Resistance|Capacitance|Impedence
     norm_method ='min_max_controls' #FC_norm | mean_norm | min_max | min_max_controls | not_normalised
-    Data = pd.read_csv(f'/Users/mo11/work/HUVEC/Data3/{norm_method}/{treatment}_Data_all_data_remapped_{measurement}.csv',index_col=0)
-    d2 = Data[Data['freq']==4000]
-    d2 = d2.drop('freq',axis=1)
-    d2.reset_index(drop=True,inplace=True)
-    d2 = d2.fillna(0)
+    Data = pd.read_csv(f'Data3/{norm_method}/{treatment}_Data_all_data_remapped_{measurement}.csv',index_col=0)
+    
+    for freq1 in list(set(Data['freq'])):
+        d2 = Data[Data['freq']==freq1]
+        d2 = d2.drop('freq',axis=1)
+        d2.reset_index(drop=True,inplace=True)
+        d2 = d2.fillna(0)
 
-    # Here loop through the spectra and calculate the slope at each point
-    # d2['E520_2'].plot()
-    idx_all = pd.Series(d2.columns)
-    
-    All_experiments = set(idx_all.str.split('_').str[0])
-    All_Experiment_Data = pd.DataFrame()
-    All_calculations = {}
-    Experiment_injury_times = {}
-    # All_experiments=['12e','1e','9e']
-    for exp1 in All_experiments:
-        # exp1='2e'
-        # exp1='16e'
-        exp1='14e'
-        # All the experiments performed together for this run
-        exp = idx_all[idx_all.str.contains(f"^{exp1}_")]
-        all_injury_times = []
-        # Detect the aproximate injury time using the EMPTY wells
-        EMPTY_WELLS = d2[list(exp[exp.str.contains('EMPTY WELL')])]
-        # experiment 8e doesnt have an empty wells in them.
+        # Here loop through the spectra and calculate the slope at each point
+        # d2['E520_2'].plot()
+        idx_all = pd.Series(d2.columns)
         
-        # EMPTY_WELLS.plot()
-        for id1 in exp:
-            # id1 =idx_all[idx_all.str.contains('E1192_1')].values[0]
-            # id1 =idx_all[idx_all.str.contains('E520_2')].values[0]
-            # id1='12e_E590_2'
-            # id1 = '12e_E588_2'
-            # id1='9e_E513_1'
-            # id1 ='14e_E659_1'
-            # id1='14e_E694_2'
-            # id1='11e_E509_2' - does not ever recover to 50%
+        All_experiments = set(idx_all.str.split('_').str[0])
+        All_Experiment_Data = pd.DataFrame()
+        All_calculations = {}
+        Experiment_injury_times = {}
+        # All_experiments=['12e','1e','9e']
+        for exp1 in All_experiments:
+            # exp1='2e'
+            # exp1='16e'
+            # exp1='11e'
+            # All the experiments performed together for this run
+            exp = idx_all[idx_all.str.contains(f"^{exp1}_")]
+            all_injury_times = []
+            # Detect the aproximate injury time using the EMPTY wells
+            EMPTY_WELLS = d2[list(exp[exp.str.contains('EMPTY WELL')])]
+            # experiment 8e doesnt have an empty wells in them.
             
-            # id1='3e_E530_1' - this fails in a capacitance analysis
-            # id1='3e_E528_2'
-            print(id1)
-            if('EMPTY WEL' in id1):
-                continue
-            
-            d1 = d2.loc[d2[id1]!= 0,id1] 
-            if measurement=='Capacitance':
-                d1 = invert(d1)
-            d3=d1.reset_index()
-            d1_index = d3['index']
-            d1=d1.reset_index(drop=True)
-            d1=d1[10:len(d1)-100]
-            injury_time_start,injury_time_end,min_value = peak_detection(d1)
-            injury_time_start_norm = d1_index[injury_time_start]
-            
-            
-            all_injury_times.append(injury_time_start_norm)
-            try:
-                Experiment_injury_times[injury_time_start_norm].append(id1)
-            except:
-                Experiment_injury_times[injury_time_start_norm]=[id1]
-            
-            Peak_window_only = d1.loc[injury_time_start:injury_time_end]
-            measurements = calculate_peak_metrics(Peak_window_only,injury_time_start,injury_time_end,min_value,d1)
-            measurements['flagged as outlier']=False
-            All_calculations[id1] = measurements
-            d1.plot()
-            
-            print('plotted')
-    
-        print('Done with this experiment')
-        Consensous_injury_start = pd.Series(all_injury_times).mode()[0]
-        
-        # We use the consensous peak measurements to detect if any of the measurements may be outliers. 
-        all_injury_times2 = pd.DataFrame(all_injury_times)
-        Q1 = all_injury_times2.quantile(0.25)
-        Q3 = all_injury_times2.quantile(0.75)
-        IQR = Q3 - Q1
-        if(IQR[0]==0):
-            # we handle the exception detection when all are the same besides 1
-            IQR = Consensous_injury_start*0.1
-        Highest_bound = Q3 +  1.5* IQR
-        Lowest_bond = Q1 - 1.5*IQR
-        
-        Outliers1 = all_injury_times2.loc[(all_injury_times2>Highest_bound)[0]]
-        Outliers2 = all_injury_times2.loc[(all_injury_times2<Lowest_bond)[0]]
-        Outliers=pd.concat([Outliers1,Outliers2])
-        # These would have had a wrong calculations performed, and may need to be repeated. Hence we repeat the calculations based on consensous peak and flag the measurement as an outlier.
-        for i,outlier in Outliers.iterrows():
-           idx = (outlier.values[0])
-           ids = Experiment_injury_times[idx]
-           
-           for id1 in ids:
+            # EMPTY_WELLS.plot()
+            for id1 in exp:
+                # id1 =idx_all[idx_all.str.contains('E1192_1')].values[0]
+                # id1 =idx_all[idx_all.str.contains('E520_2')].values[0]
+                # id1='12e_E590_2'
+                # id1 = '12e_E588_2'
+                # id1='9e_E513_1'
+                # id1 ='14e_E659_1'
+                # id1='14e_E694_2'
+                # id1='11e_E509_2' - does not ever recover to 50%
+                
+                # id1='3e_E530_1' - this fails in a capacitance analysis
+                # id1='3e_E528_2'
+                # id1='11e_E509_2'
+                print(id1)
+                if('EMPTY WEL' in id1):
+                    continue
+                
                 d1 = d2.loc[d2[id1]!= 0,id1] 
                 if measurement=='Capacitance':
                     d1 = invert(d1)
                 d3=d1.reset_index()
                 d1_index = d3['index']
                 d1=d1.reset_index(drop=True)
-                d1.plot()
-                d1=d1[Consensous_injury_start-200:Consensous_injury_start+300]
-                d1.plot()
+                d1=d1[10:len(d1)-100]
+                # d1.plot()
                 injury_time_start,injury_time_end,min_value = peak_detection(d1)
                 injury_time_start_norm = d1_index[injury_time_start]
-                # if the new injury start time fails to be in the correct place (±10% of Consensous injury time), there is something wrong experimentally and sample may need to be repeated.
-                Tolerance_Range = Consensous_injury_start*0.1
                 
-                if (Consensous_injury_start-Tolerance_Range<=injury_time_start_norm<=Consensous_injury_start+Tolerance_Range):
-                    Peak_window_only = d1.loc[injury_time_start:injury_time_end]
-                    measurements = calculate_peak_metrics(Peak_window_only,injury_time_start,injury_time_end,min_value,d1)
-                else:
-                    measurements = {'slope_of_recovery':0,'slope_val_after_treatment':0,'slope_val_before_treatment':0, 'area trapz':0, 'area simpson':0, 'recovery_time':0,'time_to_recover_to_50':0}
-
-                measurements['flagged as outlier']=True
+                
+                all_injury_times.append(injury_time_start_norm)
+                try:
+                    Experiment_injury_times[injury_time_start_norm].append(id1)
+                except:
+                    Experiment_injury_times[injury_time_start_norm]=[id1]
+                
+                Peak_window_only = d1.loc[injury_time_start:injury_time_end]
+                measurements = calculate_peak_metrics(Peak_window_only,injury_time_start,injury_time_end,min_value,d1)
+                measurements['flagged as outlier']=False
                 All_calculations[id1] = measurements
-
+                # d1.plot()
+                
+                print('plotted')
         
-        normalised_Peaks = d2.loc[Consensous_injury_start:Consensous_injury_start+200,exp]
-        # normalised_Peaks.plot()
-        normalised_Peaks=normalised_Peaks.reset_index(drop=True)
-        scaled_to_0_normalised_Peaks = scale_to_0(normalised_Peaks)
-        scaled_to_0_normalised_Peaks.plot()
-        All_Experiment_Data = pd.concat([All_Experiment_Data,scaled_to_0_normalised_Peaks], axis=1)
+            print('Done with this experiment')
+            Consensous_injury_start = pd.Series(all_injury_times).mode()[0]
+            
+            # We use the consensous peak measurements to detect if any of the measurements may be outliers. 
+            all_injury_times2 = pd.DataFrame(all_injury_times)
+            Q1 = all_injury_times2.quantile(0.25)
+            Q3 = all_injury_times2.quantile(0.75)
+            IQR = Q3 - Q1
+            if(IQR[0]==0):
+                # we handle the exception detection when all are the same besides 1
+                IQR = Consensous_injury_start*0.1
+            Highest_bound = Q3 +  1.5* IQR
+            Lowest_bond = Q1 - 1.5*IQR
+            
+            Outliers1 = all_injury_times2.loc[(all_injury_times2>Highest_bound)[0]]
+            Outliers2 = all_injury_times2.loc[(all_injury_times2<Lowest_bond)[0]]
+            Outliers=pd.concat([Outliers1,Outliers2])
+            # These would have had a wrong calculations performed, and may need to be repeated. Hence we repeat the calculations based on consensous peak and flag the measurement as an outlier.
+            for i,outlier in Outliers.iterrows():
+                idx = (outlier.values[0])
+                ids = Experiment_injury_times[idx]
+                
+                for id1 in ids:
+                        d1 = d2.loc[d2[id1]!= 0,id1] 
+                        if measurement=='Capacitance':
+                            d1 = invert(d1)
+                        d3=d1.reset_index()
+                        d1_index = d3['index']
+                        d1=d1.reset_index(drop=True)
+                        # d1.plot()
+                        d1=d1[Consensous_injury_start-200:Consensous_injury_start+400]
+                        # d1.plot()
+                        injury_time_start,injury_time_end,min_value = peak_detection(d1)
+                        injury_time_start_norm = d1_index[injury_time_start]
+                        # if the new injury start time fails to be in the correct place (±10% of Consensous injury time), there is something wrong experimentally and sample may need to be repeated.
+                        Tolerance_Range = Consensous_injury_start*0.1
+                        
+                        if (Consensous_injury_start-Tolerance_Range<=injury_time_start_norm<=Consensous_injury_start+Tolerance_Range):
+                            Peak_window_only = d1.loc[injury_time_start:injury_time_end]
+                            measurements = calculate_peak_metrics(Peak_window_only,injury_time_start,injury_time_end,min_value,d1)
+                        else:
+                            measurements = {'slope_of_recovery':0,'slope_val_after_treatment':0,'slope_val_before_treatment':0, 'area trapz':0, 'area simpson':0, 'recovery_time':0,'time_to_recover_to_50':0}
+
+                        measurements['flagged as outlier']=True
+                        All_calculations[id1] = measurements
+
+            normalised_Peaks = d2.loc[Consensous_injury_start:Consensous_injury_start+400,exp]
+            # normalised_Peaks.plot()
+            normalised_Peaks=normalised_Peaks.reset_index(drop=True)
+            scaled_to_0_normalised_Peaks = scale_to_0(normalised_Peaks)
+            # scaled_to_0_normalised_Peaks.plot()
+            All_Experiment_Data = pd.concat([All_Experiment_Data,scaled_to_0_normalised_Peaks], axis=1)
+            # All_Experiment_Data.plot()
+            print('plotted')
+        All_calculations = pd.DataFrame(All_calculations).T
+        cols1 = pd.Series(All_Experiment_Data.columns)
+        # slope_before_treatment
+        # slope after treatment
         # All_Experiment_Data.plot()
-        print('plotted')
-    All_calculations = pd.DataFrame(All_calculations).T
-    cols1 = pd.Series(All_Experiment_Data.columns)
-    # slope_before_treatment
-    # slope after treatment
-    All_Experiment_Data.plot()
-    All_Experiment_Data.to_csv(f'Data3/{norm_method}/Data_Extracted_Peaks/Extracted_Peaks_{measurement}.csv')
-    All_calculations.to_csv(f'Data3/{norm_method}/Data_Extracted_Peaks/Metrics_Calculations_{measurement}.csv')
-    control_samples = All_Experiment_Data[cols1[cols1.str.contains('CONTROL')]]
-    control_samples.plot()
+        All_Experiment_Data.to_csv(f'Data3/{norm_method}/Data_Extracted_Peaks/{treatment}_Extracted_Peaks_{freq1}_{measurement}.csv')
+        All_calculations.to_csv(f'Data3/{norm_method}/Data_Extracted_Peaks/{treatment}_Metrics_Calculations_{freq1}_{measurement}.csv')
+        control_samples = All_Experiment_Data[cols1[cols1.str.contains('CONTROL')]]
+        # control_samples.plot()
     # print(pd.DataFrame(slope_vals))
     
         
